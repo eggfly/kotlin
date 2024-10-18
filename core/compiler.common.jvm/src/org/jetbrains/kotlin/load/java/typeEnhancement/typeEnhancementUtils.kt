fda35196e994ce8e2418de1314c53ff85d55788a
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-10-18 16:43:10 +0200

    [Java interop] Fix nullability qualifier computation with overrides
    In the given test, the parameter of `ConcurrentHashMap.put`
    is annotated as not-null using an external annotation, meaning
    its qualifier for enhancement is
    {nullability=NOT_NULL, definitelyNotNull=true,
    isNullabilityQualifierForWarning=true}.
    To compute the final qualifier for the parameter of
    `ConcurrentHashMap.put`, we consider its own qualifier and the
    qualifiers of its overrides.
    ConcurrentHashMap indirectly @PurelyImplements MutableMap, so
    among `put`'s overrides is `MutableMap.put` whose parameter is
    non-flexible `K`.
    From this we derive the qualifier {nullability=NOT_NULL,
    definitelyNotNull=false, isNullabilityQualifierForWarning=false}.
    Before this commit, the algorithm for consolidating the qualifiers
    looked at the `nullability` and `definitelyNotNull` values in isolation
    and produced {nullability=NOT_NULL, definitelyNotNull=true,
    isNullabilityQualifierForWarning=false} because there was _some_
    enhancement to NOT_NULL with error severity and _some_ qualifier was
    `definitelyNotNull`.
    In the given example, this led to an error-severity type mismatch error.
    In this commit, the algorithm is reworked to distinguish NOT_NULL
    and DEFINITELY_NOT_NULL and compute the finer-grained nullability in one
    step.
    The nullability is computed for error and warning severity, and in the
    case of stateForWarnings == DEFINITELY_NOT_NULL &&
    stateForErrors == NOT_NULL, we return DEFINITELY_NOT_NULL with warning
    severity.
    
    #KT-67560 Fixed

--- core/compiler.common.jvm/src/org/jetbrains/kotlin/load/java/typeEnhancement/typeEnhancementUtils.kt
+++ core/compiler.common.jvm/src/org/jetbrains/kotlin/load/java/typeEnhancement/typeEnhancementUtils.kt
@@ -9,50 +9,99 @@ import org.jetbrains.kotlin.load.java.JvmAnnotationNames
 import org.jetbrains.kotlin.types.TypeSystemCommonBackendContext
 import org.jetbrains.kotlin.types.model.KotlinTypeMarker
 
-private fun <T : Any> Set<T>.select(low: T, high: T, own: T?, isCovariant: Boolean): T? {
+/**
+ * Version of [NullabilityQualifier] that distinguishes between NOT_NULL and DEFINITELY_NOT_NULL.
+ */
+private enum class NullabilityState(val nullability: NullabilityQualifier, val definitelyNotNull: Boolean) {
+    FORCE_FLEXIBILITY(NullabilityQualifier.FORCE_FLEXIBILITY, false),
+    NULLABLE(NullabilityQualifier.NULLABLE, false),
+    NOT_NULL(NullabilityQualifier.NOT_NULL, false),
+    DEFINITELY_NOT_NULL(NullabilityQualifier.NOT_NULL, true),
+}
+
+private fun JavaTypeQualifiers.toNullabilityState(): NullabilityState? {
+    val nullability = nullability ?: return null
+    return when (nullability) {
+        NullabilityQualifier.FORCE_FLEXIBILITY -> NullabilityState.FORCE_FLEXIBILITY
+        NullabilityQualifier.NULLABLE -> NullabilityState.NULLABLE
+        NullabilityQualifier.NOT_NULL if !definitelyNotNull -> NullabilityState.NOT_NULL
+        NullabilityQualifier.NOT_NULL -> NullabilityState.DEFINITELY_NOT_NULL
+    }
+}
+
+/**
+ * If [isCovariant] is `true`, selects the maximum element (as defined by the natural order) between [this] and [own],
+ * unless `supertypeQualifier > own` in which case `null` is returned.
+ *
+ * Otherwise, returns the single element of `this union own` or `null`.
+ */
+private fun <T : Comparable<T>> Set<T>.select(own: T?, isCovariant: Boolean): T? {
     if (isCovariant) {
-        val supertypeQualifier = if (low in this) low else if (high in this) high else null
-        return if (supertypeQualifier == low && own == high) null else own ?: supertypeQualifier
+        val supertypeQualifier = maxOrNull()
+        //In a case like supertypeQualifier = NOT_NULL, own = NULLABLE, return null
+        return if (own != null && supertypeQualifier != null && supertypeQualifier > own) null else own ?: supertypeQualifier
     }
 
     // isInvariant
-    val effectiveSet = own?.let { (this + own).toSet() } ?: this
+    val effectiveSet: Set<T> = own?.let { this + own } ?: this
     // if this set contains exactly one element, it is the qualifier everybody agrees upon,
     // otherwise (no qualifiers, or multiple qualifiers), there's no single such qualifier
     // and all qualifiers are discarded
     return effectiveSet.singleOrNull()
 }
 
-private fun Set<NullabilityQualifier>.select(own: NullabilityQualifier?, isCovariant: Boolean) =
-    if (own == NullabilityQualifier.FORCE_FLEXIBILITY)
-        NullabilityQualifier.FORCE_FLEXIBILITY
+private fun Set<NullabilityState>.select(own: NullabilityState?, isCovariant: Boolean): NullabilityState? {
+    return if (own == NullabilityState.FORCE_FLEXIBILITY)
+        NullabilityState.FORCE_FLEXIBILITY
     else
-        select(NullabilityQualifier.NOT_NULL, NullabilityQualifier.NULLABLE, own, isCovariant)
+        select<NullabilityState>(own, isCovariant)
+}
 
-private val JavaTypeQualifiers.nullabilityForErrors: NullabilityQualifier?
-    get() = if (isNullabilityQualifierForWarning) null else nullability
+private val JavaTypeQualifiers.stateForErrors: NullabilityState?
+    get() = if (isNullabilityQualifierForWarning) null else toNullabilityState()
 
+private val JavaTypeQualifiers.stateForWarnings: NullabilityState?
+    get() = if (!isNullabilityQualifierForWarning) null else toNullabilityState()
+
+/**
+ * Computes the [JavaTypeQualifiers] from the immediate and the overridden qualifiers.
+ *
+ * Error and warning qualifiers are computed separately, and the stricter of the two is selected.
+ *
+ * In the case when we have `NOT_NULL` for error and `DEFINITELY_NOT_NULL` for warning, we return `DEFINITELY_NOT_NULL` for warning.
+ * This is a bit controversial because as a result, we "lose" the enhancement from `T!` to `T` with error severity.
+ */
 fun JavaTypeQualifiers.computeQualifiersForOverride(
     superQualifiers: Collection<JavaTypeQualifiers>,
     isCovariant: Boolean,
     isForVarargParameter: Boolean,
-    ignoreDeclarationNullabilityAnnotations: Boolean
+    ignoreDeclarationNullabilityAnnotations: Boolean,
 ): JavaTypeQualifiers {
-    val newNullabilityForErrors = superQualifiers.mapNotNull { it.nullabilityForErrors }.toSet()
-        .select(nullabilityForErrors, isCovariant)
-    val newNullability = newNullabilityForErrors ?: superQualifiers.mapNotNull { it.nullability }.toSet()
-        .select(nullability, isCovariant)
-    val newMutability = superQualifiers.mapNotNull { it.mutability }.toSet()
-        .select(MutabilityQualifier.MUTABLE, MutabilityQualifier.READ_ONLY, mutability, isCovariant)
+    val stateForErrors = superQualifiers.mapNotNullTo(mutableSetOf()) { it.stateForErrors }
+        .select(stateForErrors, isCovariant)
+    val stateForWarnings = superQualifiers.mapNotNullTo(mutableSetOf()) { it.stateForWarnings }
+        .select(stateForWarnings, isCovariant)
+
+    val (newNullability, forWarning) = when {
+        stateForErrors != null && stateForWarnings != null -> maxOf(stateForErrors, stateForWarnings).let { it to (it != stateForErrors) }
+        stateForErrors != null -> stateForErrors to false
+        else -> stateForWarnings to (stateForWarnings != null)
+    }
+
     // Vararg value parameters effectively have non-nullable type in Kotlin
     // and having nullable types in Java may lead to impossibility of overriding them in Kotlin
     val realNullability = newNullability?.takeUnless {
-        ignoreDeclarationNullabilityAnnotations || (isForVarargParameter && it == NullabilityQualifier.NULLABLE)
+        ignoreDeclarationNullabilityAnnotations || (isForVarargParameter && it == NullabilityState.NULLABLE)
     }
+
+    val newMutability = superQualifiers.mapNotNullTo(mutableSetOf()) { it.mutability }
+        .select(mutability, isCovariant)
+
     return JavaTypeQualifiers(
-        realNullability, newMutability,
-        realNullability == NullabilityQualifier.NOT_NULL && (definitelyNotNull || superQualifiers.any { it.definitelyNotNull }),
-        realNullability != null && newNullabilityForErrors != newNullability
+        nullability = realNullability?.nullability,
+        definitelyNotNull = realNullability?.definitelyNotNull == true,
+        isNullabilityQualifierForWarning = forWarning,
+        mutability = newMutability,
     )
 }
 
