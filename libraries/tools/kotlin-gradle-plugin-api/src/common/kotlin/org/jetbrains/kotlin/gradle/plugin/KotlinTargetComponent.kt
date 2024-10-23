37229d0c4ec819332f025432b97d885404afa68b
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-10-23 11:21:04 +0200

    [Gradle] Add KDoc for KotlinTargetComponent
    ^KT-58858 In Progress

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTargetComponent.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTargetComponent.kt
@@ -9,14 +9,48 @@ import org.gradle.api.artifacts.PublishArtifact
 import org.gradle.api.component.SoftwareComponent
 
 /**
- * @suppress TODO: KT-58858 add documentation
+ * Special type of [SoftwareComponent] to define how and what to publish in maven repository for Kotlin projects.
+ *
+ * [KotlinTargetComponent] is an internal Kotlin Gradle Plugin representation/implementation of Gradle's [SoftwareComponent]
+ * and is subject of removing from public API.
+ *
+ * Check [this Kotlin issue](https://youtrack.jetbrains.com/issue/KT-58830) regarding the future state of this API.
  */
 interface KotlinTargetComponent : SoftwareComponent {
+
+    /**
+     * The [Kotlin target][KotlinTarget] to which this component belongs.
+     */
     val target: KotlinTarget
+
+    /**
+     * Indicates whether this [KotlinTargetComponent] can be published.
+     *
+     * @see [KotlinTarget.publishable]
+     */
     val publishable: Boolean
+
+    /**
+     * Indicates whether the current [KotlinTargetComponent] can be published on the current host.
+     *
+     * This value is determined based on the host-specific criteria and the characteristics
+     * of the Kotlin target associated with the component.
+     *
+     * @see [KotlinTarget.publishable]
+     */
     val publishableOnCurrentHost: Boolean
+
+    /**
+     * Defines the default artifact ID for the Kotlin target component.
+     *
+     * This property represents the identifier used to uniquely distinguish
+     * the primary artifact associated with this component in a Maven repository.
+     */
     val defaultArtifactId: String
 
+    /**
+     * @suppress
+     */
     @Deprecated(
         message = "Sources artifacts are now published as separate variant " +
                 "use target.sourcesElementsConfigurationName to obtain necessary information",
